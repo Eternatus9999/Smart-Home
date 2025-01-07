c7723ca729c9fe08b6b48d299e3cf62e4191c6e4
Eternatus9999 <eternatus9999@gmail.com>
2025-01-08 02:19:03 +0530

    Add Information when starting the project on different devices

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:iot_project/pages/Menu.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,); //For Website
+  await Firebase.initializeApp(); //For Mobile app
   runApp(MyApp());
 }
 
